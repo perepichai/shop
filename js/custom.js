c5c6329edd581ba532c649f30a1d3591b8034868
chepon <o.perepichai@gmail.com>
2015-10-07 00:22:22 +0300

    add loDash: load products, menu category

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,37 @@
 window.addEventListener('load', function() {
     var products = [];
+    var productsSort = [];
     var xhr = new XMLHttpRequest;
-    xhr.open('get', 'js/products.json');
+    xhr.open('GET', 'js/products.json');
     xhr.send();
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState != 4) return;
 
         if (xhr.status != 200) {
-            alert(xhr.status + ': ' + xhr.statusText);
+            console.log(xhr.status + ': ' + xhr.statusText);
         } else {
+            products = JSON.parse(xhr.responseText);
+            for(var i=0;i<products.length;i++) {
+                productsSort[i] = (products[i].category);
+            }
+            //Удаляем дубли категорий
+            productsSort = _.uniq(productsSort);
+            // -- Удаляем дубли категорий
+
             //Подключение Lodash _.template
-
-            var tmpl = document.getElementById('products').innerHTML.trim();
-            tmpl = _.template(tmpl);
-
-
-            document.getElementById('main-content').innerHTML = tmpl({
-                list: JSON.parse(xhr.responseText)
+            var tmplProducts = document.getElementById('products').innerHTML.trim();
+            tmplProducts = _.template(tmplProducts);
+            document.getElementById('main-content').innerHTML = tmplProducts({
+                list: products
             });
+            var tmplList = document.getElementById('category').innerHTML.trim();
+            tmplList = _.template(tmplList);
+            document.getElementById('list-group').innerHTML = tmplList({
+                cat: productsSort
+            });
+            // -- Подключение Lodash _.template
         }
 
     };
-
 });
