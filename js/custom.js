977bbc16085a9717c217f1a1f21066023b71640b
chepon <o.perepichai@gmail.com>
2015-10-07 01:11:41 +0300

    add loDash, load products, category, brands

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function() {
     var products = [];
     var productsSort = [];
+    var brandsSort = [];
     var xhr = new XMLHttpRequest;
     xhr.open('GET', 'js/products.json');
     xhr.send();
@@ -14,22 +15,35 @@ window.addEventListener('load', function() {
             products = JSON.parse(xhr.responseText);
             for(var i=0;i<products.length;i++) {
                 productsSort[i] = (products[i].category);
+                brandsSort[i] = (products[i].brand);
             }
-            //Удаляем дубли категорий
+            //Удаляем дубли категорий, брендов
             productsSort = _.uniq(productsSort);
-            // -- Удаляем дубли категорий
+            brandsSort = _.uniq(brandsSort);
+            // -- Удаляем дубли категорий, брендов
 
             //Подключение Lodash _.template
+            //Products
             var tmplProducts = document.getElementById('products').innerHTML.trim();
             tmplProducts = _.template(tmplProducts);
             document.getElementById('main-content').innerHTML = tmplProducts({
                 list: products
             });
+            //Category
             var tmplList = document.getElementById('category').innerHTML.trim();
             tmplList = _.template(tmplList);
-            document.getElementById('list-group').innerHTML = tmplList({
+            var tmplBrand = document.getElementById('brand').innerHTML.trim();
+            tmplBrand = _.template(tmplBrand);
+            document.getElementById('list-group-category').innerHTML = tmplList({
                 cat: productsSort
             });
+            document.getElementById('list-group-category').innerHTML += tmplBrand({
+                brand: brandsSort
+            });
+            //Brands
+            //document.getElementById('list-group-category').innerHTML += tmplBrand({
+            //    brand: brandsSort
+            //});
             // -- Подключение Lodash _.template
         }
 
